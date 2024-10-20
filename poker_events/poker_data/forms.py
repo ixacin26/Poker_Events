346a876863a67f8f9fde8917bf3d58655790027a
ixacin26 <klaus.tauber@gmx.net>
2024-10-20 20:00:39 +0200

    Database extended

---
+++ poker_events/poker_data/forms.py
@@ -0,0 +1,2 @@
+#TODO Player delete while creating new event - disable
+#TODO Event only to choose when activ
--- poker_events/poker_data/models.py
+++ poker_events/poker_data/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
@@ -11,7 +12,7 @@ class Player(models.Model):
 
 class Event(models.Model):
     host_location = models.CharField(max_length=100, blank=True, null=True)  # formerly "gastgeber_ort"
-    participants = models.ManyToManyField(Player, through='EventParticipation')  # formerly "teilnehmer"
+    participants = models.ManyToManyField(Player, through='EventParticipation', related_name='participations')  # formerly "teilnehmer"
     pot = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()  # formerly "datum"
     active = models.BooleanField(default=True)  # formerly "active"
@@ -19,8 +20,13 @@ class Event(models.Model):
     host_player = models.ForeignKey(Player, on_delete=models.SET_NULL, blank=True, null=True, related_name='hosted_events')  # formerly "gastgeber_spieler"
 
     def clean(self):
+        # Custom validation to ensure either `asop` is True or `gastgeber_spieler` is provided
         if not self.asop and not self.host_player:
-            raise ValidationError("Either a host player or ASOP event must be provided.")
+            raise ValidationError("Entweder muss ASOP aktiviert sein oder ein Spieler ausgewählt werden.")
+        elif self.asop and self.host_player:
+            raise ValidationError("Entweder ASOP aktivieren oder einen Spieler auswählen, aber nicht beides.")
+        elif not self.asop and self.host_player is None:
+            raise ValidationError("Wenn ASOP nicht aktiviert ist, muss ein Gastgeber-Spieler ausgewählt werden.")
 
     def __str__(self):
         return f"Event on {self.date} with Pot: {self.pot}"
