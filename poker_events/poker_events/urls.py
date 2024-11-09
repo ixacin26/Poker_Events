76712942191197f73e4bcaecd292e490444401eb
ixacin26 <klaus.tauber@gmx.net>
2024-11-09 14:42:53 +0100

    basic landing page in place

--- poker_events/poker_events/urls.py
+++ poker_events/poker_events/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from poker_events import views  # Import views from the appropriate directory
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Add this line
 ]
